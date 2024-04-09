3b9eadfcda72e03bd60c280a66f5cb0d660207cb
David Ayme <dayme1937@uta.edu.ec>
2024-04-09 18:30:06 -0500

    Eliminar archivos y actualizar importaciones en app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PersonaModule } from './persona/persona.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PersonaModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
